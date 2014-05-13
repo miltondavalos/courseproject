a26eb2ccb39a19d875b17fe29f2b7cb1067bc8e0
miltondavalos <miltondavalos@gmail.com>
2014-05-12 22:20:38 -0400

    10 - 17

---
+++ precourse/exercises/10.rb
@@ -0,0 +1,7 @@
+#Can hash values be arrays? Can you have an array of hashes? (give examples)
+
+#yes
+
+hash_array = {name: ['maria','smith']}
+
+array_of_hashes = [{:name=>'michael'},{:name=>'ronald'}]
