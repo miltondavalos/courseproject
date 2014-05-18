f8295b5500b1e022be3bc01dcfaf565e60106c5f
miltondavalos <miltondavalos@gmail.com>
2014-05-18 00:24:11 -0400

    cleanup

--- blackjack_game_lesson_1/blackjack_game.rb
+++ blackjack_game_lesson_1/blackjack_game.rb
@@ -12,7 +12,7 @@ end
 class String
   def to_blackjack
     if self.downcase == 'ace'
-      1#[1,11]
+      'A'
     elsif self.downcase == 'jack'
       10
     elsif self.downcase == 'queen'
@@ -29,7 +29,10 @@ def transition(time=1)
   sleep time
 end
 
-def who_wins(player_count, dealer_count)
+def determine_winner(result)
+  player_count = result[:player_total]
+  dealer_count = result[:dealer_total]
+
   if player_count > 21
     puts "YOU LOST! -- #{player_count} vs. #{dealer_count}"
   elsif player_count == dealer_count
@@ -42,95 +45,99 @@ def who_wins(player_count, dealer_count)
     puts "YOU WIN! -- #{player_count} vs. #{dealer_count}"
   end
 end
-puts "what is your name?"
-name = gets.chomp
 
-puts "Hi, #{name}"
-puts 'Are you ready to play blackjack? (Y/N)'
-ready = gets.chomp
+def dealing_to(who, deck)
+  hit = deal(deck)
+  puts '----------------------------------'
+  puts "#{who.upcase}: #{hit.join(' of ')}"
+  puts '----------------------------------'
+  transition
+  hit.first.to_blackjack
+end
 
-if ready.upcase != 'Y'
+def your_score(player_total)
+  puts '----------------------------------'
+  puts "YOUR SCORE: #{total(player_total)}"
+  puts '----------------------------------'
+end
+
+def total(cards)
+  counter = 0
+  cards.each do |card|
+    if card == 'A'
+      counter += 11
+    else
+      counter += card
+    end
+  end
+  cards.select{|e| e == "A"}.count.times{counter -= 10 if counter > 21}
+  counter
+end
+
+def new_deck_of_cards
+  puts "Got a new deck of cards"
+  new_deck
+end
+
+def shuffle(deck)
+  puts "Shuffling"
+  deck.shuffle!.shuffle!.shuffle!
+end
+
+def next_action(player_total, dealer_total, deck)
+  next_action = 'hit'
+  while next_action.downcase == 'hit' && total(player_total) < 22 do
+    puts "What do you want to do? (hit/stay)"
+    transition
+    next_action = gets.chomp
+    if next_action.downcase == 'hit'
+      player_total << dealing_to('you', deck)
+      your_score(player_total)
+    else
+      while total(dealer_total) <= 17 do
+        dealer_total  << dealing_to('dealer', deck)
+      end
+    end
+  end
+  {player_total: total(player_total), dealer_total: total(dealer_total)}
+end
+
+def exit_game
   puts "Ok, have a great day."
   exit
 end
 
-puts "Getting a new deck of cards"
-deck = new_deck
-transition
-puts "Shuffling"
-deck = deck.shuffle
-
-$dealer_count = 0
-$player_count = 0
-
-def dealing_to(who, deck)
-  counter = eval("$#{who}_count")
-  hit = deal(deck)
-  counter += hit.first.to_blackjack
-  puts "#{who.upcase}: #{hit.join(' of ')} COUNT: #{counter}"
-  transition
-  eval("$#{who}_count = #{counter}")
-  #hit = deal(deck)
-  #counter += hit.first.to_blackjack
-  #puts "#{who.upcase}: #{hit.join(' of ')} COUNT: #{counter}"
-  #transition
-end
-#---------------------------------------------------------------------
-#dealer_count = 0
-#hit = deal(deck)
-#dealer_count += hit.first.to_blackjack
-#puts "DEALER: #{hit.join(' of ')} COUNT: #{dealer_count}"
-#transition
-#hit = deal(deck)
-#dealer_count += hit.first.to_blackjack
-#puts "DEALER: #{hit.join(' of ')} COUNT: #{dealer_count}"
-#transition
-dealing_to('dealer', deck)
-puts  "#{$dealer_count}"
-dealing_to('dealer', deck)
-dealer_count = $dealer_count
-#------------------------------------------------------------------------
-player_count = 0
-hit = deal(deck)
-player_count += hit.first.to_blackjack
-puts "#{name.upcase}: #{hit.join(' of ')} COUNT: #{player_count}"
-transition
-hit = deal(deck)
-player_count += hit.first.to_blackjack
-puts "#{name.upcase}: #{hit.join(' of ')} COUNT: #{player_count}"
-transition
-#-------------------------------------------------------------------------
-next_action = 'hit'
-while next_action.downcase == 'hit' && player_count < 22 do
-  puts "What do you want to do? (hit/stay)"
-  transition
-  next_action = gets.chomp
-  if next_action.downcase == 'hit'
-    hit = deal(deck)
-    player_count += hit.first.to_blackjack
-    puts "#{name.upcase}: #{hit.join(' of ')} COUNT: #{player_count}"
-    transition
-  else
-    #puts "DEALER: #{dealer_count} vs. #{name.upcase}: #{player_count}"
-    while ((dealer_count < player_count) && (dealer_count << 22)) || (dealer_count < player_count) do
-      hit = deal(deck)
-      dealer_count += hit.first.to_blackjack
-      puts "DEALER: #{hit.join(' of ')} COUNT: #{dealer_count}"
-      transition
-    end
-  end
+def play
+  @deck = nil
+  dealer_total = []
+  player_total = []
+  @deck = shuffle(new_deck_of_cards)
+  dealer_total << dealing_to('dealer', @deck)
+  player_total << dealing_to('you', @deck)
+  dealer_total << dealing_to('dealer', @deck)
+  player_total << dealing_to('you', @deck)
+  your_score(player_total)
+  result = next_action(player_total, dealer_total, @deck)
+  determine_winner(result)
 end
 
-who_wins(player_count, dealer_count)
+
+puts "what is your name?"
+name = gets.chomp
+puts "Hi, #{name}"
+puts 'Are you ready to play blackjack? (Y/N)'
+ready = gets.chomp
+exit_game if ready.upcase != 'Y'
+play
 
 play_again = 'y'
 while play_again.downcase == 'y'
   puts "Do you want to play again? (Y/N)"
   play_again = gets.chomp
   if play_again.downcase == 'y'
-    puts "TODO...need to write code..."
+    play
   else
-    puts "bye bye"
+    puts "Come again soon!"
   end
 end
 
